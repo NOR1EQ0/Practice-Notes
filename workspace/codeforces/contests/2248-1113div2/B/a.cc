dd2dfd8c5768c707d59c1dfe5b232838fbc1fe8a
NOR1EQ0 <301385613+NOR1EQ0@users.noreply.github.com>
2026-08-02 00:42:47 +0800

    codeforces contest2248 add, A AC

---
+++ workspace/codeforces/contests/2248-1113div2/B/a.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
+static inline void solve() {
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+    
+}
+int main() {
+#if defined(ONLINE_JUDGE) || 1
+    ios::sync_with_stdio(0), cin.tie(0);
+#elif 10
+    freopen("1.in", "rb", stdin);
+#endif
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
