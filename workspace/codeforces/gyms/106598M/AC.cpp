a1038a2b6bcba9f5ed370157539e943827935ae8
NOR1EQ0 <301385613+NOR1EQ0@users.noreply.github.com>
2026-09-08 19:45:16 +0800

    codeforces 106598M AC. (follow yawn's solution)

--- workspace/codeforces/gyms/106598M/AC.cpp
+++ workspace/codeforces/gyms/106598M/AC.cpp
@@ -7,6 +7,21 @@ static inline void solve() {
     string s, t;
     ll p, q;
     cin >> s >> t >> p >> q;
+    if (!p || !q) {
+        cout << '0';
+    } else {
+        string s1 = s + t,
+               s2 = t + s;
+        auto order = s1 <=> s2;
+        if (order < 0) {
+            cout << '<';
+        } else if (order == 0) {
+            cout << '=';
+        } else {
+            cout << '>';
+        }
+    }
+    cout << '\n';
 }
 int main() {
 #if defined(ONLINE_JUDGE) || 1
