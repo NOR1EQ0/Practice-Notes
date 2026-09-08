cd10a5b376f728551f3f0f644c4ca93f00d64683
NOR1EQ0 <301385613+NOR1EQ0@users.noreply.github.com>
2026-09-08 13:50:40 +0800

    codeforces 106598M add

--- workspace/nowcoder/tracker/skill-tree/ch2/dp-intro/数字三角形/a.cc
+++ workspace/nowcoder/tracker/skill-tree/ch2/dp-intro/数字三角形/a.cc
@@ -4,24 +4,29 @@ using ll = long long;
 using ull = unsigned long long;
 constexpr ll MOD = 998244353;
 
-static inline ptrdiff_t rootl(ptrdiff_t i) {
-    if (!i) {
-        return numeric_limits<ptrdiff_t>::max();
-    }
-    auto lside =
-}
-
-static inline ptrdiff_t rootr(ptrdiff_t i) {
-}
-
-ll dp[505];
+ll hill[505][505];
 static inline void solve() {
     ll n;
     cin >> n;
-    auto hill_size = (1 + n) * n / 2;
-    vector<ll> hill(hill_size);
-    dp[0] = hill[0];
-    
+    ll mx = LLONG_MIN;
+    for (ll i = 0; i < n; i++) {
+        for (ll j = 0; j < i + 1; j++) {
+            cin >> hill[i][j];
+            if (i == 0) {
+                mx = hill[i][j];
+                continue;
+            }
+            if (j == 0) {
+                hill[i][j] += hill[i - 1][j];
+            } else if (j == i) {
+                hill[i][j] += hill[i - 1][j - 1];
+            } else {
+                hill[i][j] += max(hill[i - 1][j - 1], hill[i - 1][j]);
+            }
+        }
+    }
+    mx = *max_element(hill[n-1], hill[n-1] + n);
+    cout << mx << '\n';
 }
 int main() {
 #if defined(ONLINE_JUDGE) || 1
