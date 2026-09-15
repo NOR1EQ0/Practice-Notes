4875b609fb08229cf0a4bc21f211948fa7165ad5
NOR1EQ0 <301385613+NOR1EQ0@users.noreply.github.com>
2026-09-15 15:34:50 +0800

    update

--- workspace/nowcoder/contests/140235-Round161/D-upsolve/a.cpp
+++ workspace/nowcoder/contests/140235-Round161/D-upsolve/a.cpp
@@ -6,17 +6,40 @@ constexpr ll MOD = 998244353;
 
 int grid[300][300];
 char buffer[300];
+
+struct point {
+    int x;
+    int y;
+    bool operator<(const point& other) {
+        return x < other.x && y < other.y;
+    }
+};
 static inline void solve() {
     ll n, m;
     cin >> n >> m;
+    bool hasone = false;
     for (int i = 1; i <= n; i++) {
         cin >> buffer;
         for (int j = 1; j <= m; j++) {
-            grid[i][j] = buffer[j - 1];
+            grid[i][j] = buffer[j - 1] - '0';
+            if (buffer[j - 1] == '1') {
+                hasone = true;
+            }
         }
     }
-
-    
+    if (!hasone) {
+        cout << "0 0 0\n";
+        return;
+    }
+    map<point, point> m4;
+    map<point, point> m8;
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (grid[i][j]) {
+                
+            }
+        }
+    }
 }
 int main() {
 #if defined(ONLINE_JUDGE) || 1
