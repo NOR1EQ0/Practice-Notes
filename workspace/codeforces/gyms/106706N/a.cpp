c9ebee1ba77d5ed9520d28b4928ad6df38e5283f
NOR1EQ0 <301385613+NOR1EQ0@users.noreply.github.com>
2026-09-18 08:16:46 +0800

    codeforces 106706N add

--- workspace/codeforces/gyms/106706N/a.cpp
+++ workspace/codeforces/gyms/106706N/a.cpp
@@ -4,7 +4,11 @@ using ll = long long;
 using ull = unsigned long long;
 constexpr ll MOD = 998244353;
 static inline void solve() {
-    
+    ll n;
+    cin >> n;
+    vector<ll> xv(n), yv(n);
+    for (auto&& i : xv) cin >> i;
+    for (auto&& i : yv) cin >> i;
 }
 int main() {
 #if defined(ONLINE_JUDGE) || 1
